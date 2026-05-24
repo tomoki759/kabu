5dc3684f629a59329cd0227e314a06db74aa5128
tomoki759 <79028621+tomoki759@users.noreply.github.com>
2026-05-24 15:49:21 +0900

    Update scrape_kabutan_260205.py

--- scrape_kabutan_260205.py
+++ scrape_kabutan_260205.py
@@ -17,6 +17,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
 HEADERS = {
     "User-Agent": (
